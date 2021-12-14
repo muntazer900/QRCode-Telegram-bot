cd8a6e5e4363dd92a7ecdaaafc2014ee588dcc5c
muntaxer922 <95924288+muntaxer922@users.noreply.github.com>
2021-12-14 19:25:43 +0300

    Update messages.py

--- messages.py
+++ messages.py
@@ -6,6 +6,6 @@
 
 class Msg:
     source = "\nsource:https://github.com/AbhijithNT/QRCode-Telegram-bot"
-    start = "\n<b>This is a QR code generator bot by @thankappan369</b>"
-    error = "something is went wrong\n{error} \ncontact admin @thankappan369"
+    start = "\n<b>This is a QR code generator bot by @IIlIIIIIll</b>"
+    error = "something is went wrong\n{error} \ncontact admin @IIlIIIIIll"
     help = "You can encode and decode QRCode\nUsage: <b>Send any URL or text, this bot can convert to QRCode.\nYou can send a QR code image and decode it.</b>"
